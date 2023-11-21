24143a84d28645094c1944b2ee791a4c910c323b
Calvin Huang <calvin.peak@casplock.tw>
2023-11-21 10:51:28 +0800

    feat: add config

---
+++ src/config/routes.tsx
@@ -0,0 +1,15 @@
+﻿import CampaignIcon from '@mui/icons-material/Campaign'
+
+export interface IRoute {
+  breadcrumbDisabled?: boolean,
+  path: string
+  absPath: string
+  file: string
+  id: string
+  parentId?: string
+  [key: string]: any
+}
+
+export interface IClientRoute extends IRoute {
+  routes?: IClientRoute[]
+}
