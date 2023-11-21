88386478b0a58fadf71ed323e0e9bf7e0756c449
Calvin Huang <calvin.peak@casplock.tw>
2023-11-22 00:57:37 +0800

    fix: use rollup to output modules in different paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,56 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import type { LibraryOptions } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import dts from 'vite-plugin-dts'
+
+import pkg from './package.json'
+
+const dependencies = Object.keys(pkg.dependencies || {})
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    dts({
+      entryRoot: 'src',
+      insertTypesEntry: true,
+      include: [
+        'src/hooks',
+        'src/config',
+        'src/base',
+        'src/pro',
+      ],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '@material-ui/icons': '@material-ui/icons/esm',
+    },
+  },
+  build: {
+    minify: false,
+    rollupOptions: {
+      preserveEntrySignatures: 'allow-extension',
+      external: (source) => dependencies.some(dep => source.startsWith(dep)),
+      input: {
+        main: 'src/index.tsx',
+        base: 'src/base/index.tsx',
+        pro: 'src/pro/index.tsx',
+        hooks: 'src/hooks/index.tsx',
+      },
+      output: {
+        format: 'es',
+        entryFileNames: '[name]/index.js',
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        },
+      },
+    },
+    target: 'esnext',
+  },
 })
